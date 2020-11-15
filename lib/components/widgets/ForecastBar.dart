7dd7ac6eff8f80bfcd56ae98dee15f9bf137825f
Guilherme Noronha da Silva <guinoronha7@gmail.com>
2020-11-15 13:57:49 -0300

    Forecast

--- lib/components/widgets/ForecastBar.dart
+++ lib/components/widgets/ForecastBar.dart
@@ -1,12 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:weatherApp/model/FuturasPrevisoes.dart';
 
 class ForecastBar extends StatelessWidget {
+  final FuturasPrevisoes previsao;
+
+  ForecastBar(
+    this.previsao
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
+        Text(previsao.data),
+        Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 3,
+            ),
+            child: Image.asset(
+              'assets/images/',
+              fit: BoxFit.cover,
+            ),
+          ),
+        Text("${previsao.tempMin} Min - ${previsao.tempMax} Max"),
+        Text(previsao.descricao),
       ],
     );
   }
 
+  retornaImage(String cond) {
+    switch(cond){
+      case "storm" {
+        }
+        break;
+      case "snow" {
+        return
+      } 
+      break;
+      case "hail" {
+        return
+      } 
+      break;
+      case "rain" {
+        return
+      } 
+      break;
+      case "fog" {
+        return
+      } 
+      break;
+      case "cloud" {
+        return
+      } 
+      break;
+      case "cloudly_day" {
+        return
+      } 
+      break;
+      case "cloudly_night" {
+        return
+      } 
+      break;
+      default {
+        if(cond == "none_day" || cond == "clear_day") {
+          return
+        } else {
+          //none_night or clear_night
+          return
+        }
+      }
+    }
+  }
 }
--- lib/components/widgets/ForecastCard.dart
+++ lib/components/widgets/ForecastCard.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:weatherApp/components/widgets/ForecastBar.dart';
+import 'package:weatherApp/model/FuturasPrevisoes.dart';
 
-class ForecastBar extends StatelessWidget {
+class ForecastCard extends StatelessWidget {
+  final List<FuturasPrevisoes> listaPrev;
+
+  ForecastCard(
+    this.listaPrev,
+  );
+  
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -9,7 +18,14 @@ class ForecastBar extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(10),
         child: Row(
-          children: [],
+          children: [
+            ForecastBar(listaPrev[1]),
+            ForecastBar(listaPrev[2]),
+            ForecastBar(listaPrev[3]),
+            ForecastBar(listaPrev[4]),
+            ForecastBar(listaPrev[5]),
+            ForecastBar(listaPrev[6]),
+          ],
         ),
       ),
     );
